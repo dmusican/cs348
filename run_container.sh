1d01672bb6f8e19a5096e24d5e547554d7eba464
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-07-20 14:43:11 -0500

    updated to pull from Docker Hub

---
+++ run_container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPO_DIR=$(pwd)
+CONTAINER_NAME=cs348-container
+COURSE_DIR=/cs348
+
+
+# Build the container if it does not already exist
+CONTAINER_LOOKUP=$(docker images $CONTAINER_NAME --format "{{.Repository}}")
+if [[ $CONTAINER_LOOKUP != $CONTAINER_NAME ]]; then
+        cd config
+        docker build -f Dockerfile -t $CONTAINER_NAME .
+        cd "$REPO_DIR"
+fi
+
+# Mount with type=bind to allow changes made
+# inside the container to be visible on the host;
+# also cd into COURSE_DIR once the container is running
+
+docker run \
+        -it \
+        --shm-size=2gb \
+        --mount src="$REPO_DIR/code",target=$COURSE_DIR,type=bind \
+        --workdir $COURSE_DIR \
+        $CONTAINER_NAME \
+        zsh
